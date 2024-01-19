636a588f929703806880404c27174e6673a678e3
agdc101 <agdc101@gmail.com>
2024-01-19 08:32:20 +0000

    feat: LL added for blog pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
 
 const BlogPage = lazy(() => import('./pages/Blog'));
+const PostPage = lazy(() => import('./pages/Post'));
 
 const router = createBrowserRouter([
   {
@@ -29,7 +29,15 @@ const router = createBrowserRouter([
             loader: () => 
               import( './pages/Blog').then(module => module.loader() ) 
           },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+
+          { path: ':id', 
+            element:             
+              <Suspense fallback={<p>loading...</p>} > 
+                <PostPage/> 
+              </Suspense>, 
+            loader: ({params}) => 
+              import( './pages/Post').then(module => module.loader({params}) ) 
+          },
         ],
       },
     ],
